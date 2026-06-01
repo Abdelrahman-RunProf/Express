82f283bd25140b3daa859298ebb6aadbb9e2599e
Abdelrahman-RunProf <development@runprof.com>
2026-06-02 01:30:40 +0300

    handle delete

--- app.js
+++ app.js
@@ -1,73 +1,99 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
 const app = express();
-const userModel = require("./modals/users");
-const port = 3000;
-const moment = require('moment'); // require
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
+const port = 3001;
+const mongoose = require("mongoose");
 app.use(express.urlencoded({ extended: true }));
+const User = require("./models/customerSchema");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+var moment = require("moment");
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+// Auto refresh
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        liveReloadServer.refresh("/");
+    }, 100);
+});
+
+// GET Requst
+app.get("/", (req, res) => {
+    // result ==> array of objects
+    console.log("--------------------------------------------");
+    User.find()
+        .then((result) => {
+            res.render("index", { arr: result, moment: moment });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.get("/user/add.html", (req, res) => {
+    res.render("user/add");
+});
+
+app.get("/edit/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then((result) => {
+            res.render("user/edit", { obj: result, moment: moment });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+
+
+
+});
+
+app.get("/view/:id", (req, res) => {
+    // result ==> object
+    User.findById(req.params.id)
+        .then((result) => {
+            res.render("user/view", { obj: result, moment: moment });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// POST Requst
+app.post("/user/add.html", (req, res) => {
+    const user = new User(req.body);
+    user
+        .save()
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// DELETE Request
+app.delete("/edit/:id", (req, res) => {
+    User.deleteOne({ _id: req.params.id })
+        .then((result) => {
+            res.redirect("/");
+            console.log(result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 mongoose.connect('mongodb+srv://abdelrahmanabdou52_db_user:TrIg0l2gVHD8t02T@cluster0.zjbhhpa.mongodb.net/all-data?appName=Cluster0')
     .then(() => app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     })).catch((error) => console.log(error))
 
-
-
-app.get('/', async (req, res) => {
-    try {
-        const users = await userModel.find();
-        res.render('index', { users, moment });
-    } catch (error) {
-        console.log('error from get', error);
-        res.render('index', { users: [] });
-    }
-});
-
-
-
-app.get('/user/add', (req, res) => {
-    res.render('user/add')
-});
-app.get('/user/edit/:id', (req, res) => {
-    res.render('user/edit')
-});
-app.get('/user/search', (req, res) => {
-    res.render('user/search')
-});
-app.get('/user/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const user = await userModel.findById(id);
-        console.log(user, 'user from view');
-        res.render('user/view', { user, moment });
-    } catch (error) {
-        console.log(error);
-        res.render('user/view', { user: null });
-    }
-});
-
-app.post("/user/add", (req, res) => {
-    const { firstName, lastName, email, telephone, age, country, gender } = req.body;
-    const userModelData = new userModel({
-        firstName,
-        lastName,
-        email,
-        telephone,
-        age,
-        country,
-        gender
-    })
-    userModelData.save()
-        .then(() => {
-            res.redirect('/')
-            console.log("user added successfully")
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
-})
-app.use((req, res) => {
-    res.status(404).render("404")
-});
--- modals/users.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = require("../schemas/userSchema");
-const userModel = mongoose.model("userModel", userSchema);
-module.exports = userModel;
